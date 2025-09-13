3bcd91e3dbf5d111a658c981f9fcd4f092aa753a
hdthinh1012 <hdthinh01@gmail.com>
2025-09-13 20:42:40 +0700

    chores: adding tailwind, typescipt

--- frontend-app/src/layouts/DefaultLayout.jsx
+++ frontend-app/src/layouts/DefaultLayout.jsx
@@ -3,6 +3,9 @@ import { Outlet } from 'react-router-dom';
 
 const DefaultLayout = () => (
   <div>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     <h1>Default Layout</h1>
     {/* Add header, sidebar, etc. here if needed */}
     <Outlet />
